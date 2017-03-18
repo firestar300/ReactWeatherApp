587d386c6b5b293baa60ab7f07c39145bda42e6c
Milan Ricoul <milan.ricoul@gmail.com>
2017-03-18 13:18:36 +0100

    Style Example page

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,8 +5,11 @@ var Main = (props) => {
   return (
     <div>
       <Nav />
-      <h2>Main Component</h2>
-      {props.children}
+      <div className="row">
+        <div className="columns medium-6 medium-offset-3 large-4 large-offset-4 small-centered">
+          {props.children}
+        </div>
+      </div>
     </div>
   );
 }
